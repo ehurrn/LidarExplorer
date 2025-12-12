36201c9f49029a6a0ed8e9e5f4c5cbd1641376fc
eric herren <eric@ericherren.com>
2025-12-11 23:01:28 -0600

    Initial Commit

---
+++ LidarExplorer/LidarExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LidarExplorerApp.swift
+//  LidarExplorer
+//
+//  Created by Eric Herren on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LidarExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
